8bf18d8366ad7d347272c888a1ef284ee6d9a2c6
Yaroslav Ilich <yar.ilich@gmail.com>
2023-07-21 15:55:31 +0300

    Implemented target handler with ws calls

--- app/net/westaystay/TargetHandler.scala
+++ app/net/westaystay/TargetHandler.scala
@@ -1,5 +1,19 @@
 package net.westaystay
 
-class TargetHandler {
-  def handleOptionalTarget(targetOpt: Option[Int]): Int = ???
+import play.api.libs.ws._
+
+import scala.concurrent.duration.DurationInt
+import scala.concurrent.{Await, ExecutionContext}
+
+class TargetHandler(ws: WSClient, defaultTarget: Int) {
+  def handleOptionalTarget(targetOpt: Option[Int])(implicit ec: ExecutionContext): Int = {
+    val request = ws.url("https://httpbin.org/get")
+    val requestWithQuery = targetOpt.fold(request)(target => request.withQueryStringParameters("target" -> target.toString))
+    val responseTarget = requestWithQuery.get().map { response =>
+      (response.json \ "args" \ "target").as[Int]
+    }
+
+    val resultingTarget = responseTarget.recover(_ => defaultTarget)
+    Await.result(resultingTarget, 2000.millis)
+  }
 }
--- build.sbt
+++ build.sbt
@@ -7,8 +7,11 @@ lazy val root = (project in file(".")).enablePlugins(PlayScala)
 
 scalaVersion := "2.13.11"
 
-libraryDependencies += guice
-libraryDependencies += "org.scalatestplus.play" %% "scalatestplus-play" % "5.1.0" % Test
+libraryDependencies ++= Seq(
+  guice,
+  ws,
+  "org.scalatestplus.play" %% "scalatestplus-play" % "5.1.0" % Test
+)
 
 // Adds additional packages into Twirl
 //TwirlKeys.templateImports += "net.westaystay.controllers._"
